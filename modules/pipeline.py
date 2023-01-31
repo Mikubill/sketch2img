fix: gradient stuff

modules/pipeline.py
@@ -3,7 +3,9 @@ from typing import Callable, List, Optional, Union
 import torch
 from diffusers.utils import logging
 from diffusers import StableDiffusionPipeline
-
+from einops import rearrange
+import contextlib
+import torch.nn.functional as F
 from modules.latent_predictor import LatentEdgePredictor, hook_unet
 
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
@@ -70,7 +72,7 @@ class AntiGradientPipeline(StableDiffusionPipeline):
             generator,
             latents,
         )
-        init_noise = latents.clone()
+        noise = latents.detach().clone()
 
         # 6. Prepare extra step kwargs. TODO: Logic should ideally just be moved out of the pipeline
         extra_step_kwargs = self.prepare_extra_step_kwargs(generator, eta)
@@ -82,18 +84,29 @@ class AntiGradientPipeline(StableDiffusionPipeline):
                 # expand the latents if we are doing classifier free guidance
                 latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
                 latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
+                latent_model_input = latent_model_input.requires_grad_(True)
+                
+                ctx = torch.enable_grad()
+                step_stop = 0.5 * len(timesteps)
+                if i > step_stop:
+                    ctx = contextlib.nullcontext()
 
                 # predict the noise residual
-                noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+                with ctx:
+                    noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
 
                 # perform guidance
                 if do_classifier_free_guidance:
                     noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-                    noise_pred_text = self.apply_anti_gradient(latents, noise_pred_text, init_noise, t, sketch_image, 1.5)
                     noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
                 # compute the previous noisy sample x_t -> x_t-1
                 latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
+                
+                # compute predictor gradient
+                with ctx:
+                    if i <= step_stop:
+                        latents = self.apply_anti_gradient(latent_model_input, latents, noise, t, sketch_image, 1.6)
 
                 # call the callback, if provided
                 if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0):
@@ -125,22 +138,24 @@ class AntiGradientPipeline(StableDiffusionPipeline):
         noise_level = sqrt_one_minus_alpha_prod.to(noise.device) * noise
         return noise_level
     
-    def apply_anti_gradient(self, input_latents, pred, noise, timestep, target, beta):
-        
+    def apply_anti_gradient(self, latents_prev, latents, noise, timestep, target, beta):
         intermediate_result = []
         for block in self.feature_blocks:
-            resized = torch.nn.functional.interpolate(block.output, size=pred.shape[2], mode="bilinear") 
+            resized = F.interpolate(block.output, size=latents.shape[2], mode="bilinear") 
             intermediate_result.append(resized)
-                    # free vram
             del block.output
                     
+        intermediate_result = torch.cat(intermediate_result, dim=1)
         estimate_noise = self.get_noise_level(noise, timestep)
-        outputs = self.lgp_model(intermediate_result, estimate_noise)
-        loss = torch.nn.functional.mse_loss(target.float(), outputs.float(), reduction="mean")
-        loss.backward()
+        outputs = self.lgp_model(intermediate_result, torch.cat([estimate_noise] * 2))
         
-        normlised = torch.dist(input_latents, pred) / inputs.grad ** 2 * beta
-        return pred
+        b, _, h, w = latents_prev.shape
+        _, outputs = rearrange(outputs, "(b w h) c -> b c h w", b=b, h=h, w=w).chunk(2)
+        loss = F.mse_loss(target.float(), outputs.float(), reduction="mean")
+            
+        _, cond_grad = (-torch.autograd.grad(loss, latents_prev)[0]).chunk(2)
+        alpha = torch.linalg.norm(latents_prev - latents) / torch.linalg.norm(cond_grad) * beta
+        return latents - alpha * cond_grad
         
         
         

trainer.py
@@ -231,15 +231,15 @@ def train():
                 # Predict the noise residual
                 unet(noisy_latents, timesteps, encoder_hidden_states)
                 
-                intermidiate_result = []
+                intermediate_result = []
                 for block in feature_blocks:
                     resized = torch.nn.functional.interpolate(block.output, size=latents.shape[2], mode="bilinear") 
                     intermidiate_result.append(resized)
                     # free vram
                     del block.output
                     
-                intermidiate_result = torch.cat(intermidiate_result, dim=1)
-                result = edge_predictor(intermidiate_result, noise_level)
+                intermidiate_result = torch.cat(intermediate_result, dim=1)
+                result = edge_predictor(intermediate_result, noise_level)
                 result = rearrange(result, "(b w h) c -> b c h w", b=bsz, h=sketchs.shape[2], w=sketchs.shape[3])
                 loss = torch.nn.functional.mse_loss(result, sketchs, reduction="mean")
 